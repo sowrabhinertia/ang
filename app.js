1518e0f5de2adc924366e6148d35d706390954f8
sowrabhinertia <sowrabhullal1998@gmail.com>
2018-10-13 20:56:33 +0530

    assignment

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 (function(){
   var app=angular.module("myapp",[]);
   app.controller("mycontroller",function($scope){
-    $scope.name="sowrabh" ;
-    $scope.hello=function(){
-      return "hello";
+    $scope.check = function() {
+      if($scope.food == null){
+        $scope.result="Please enter data first";
+      }
+      else{
+      var str=$scope.food;
+      var str_array = str.split(',');
+      if(str_array.length <= 3){
+        $scope.result="Enjoy!"
+      }
+      else{
+        $scope.result="Too much!"
+      }
     }
+}
+
   });
 })();
